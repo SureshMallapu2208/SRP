3efea8375d458df571af85283ebd1b37638b9503
m.suresh <m.suresh@LTPBAN252476763>
2023-08-23 11:19:47 +0530

    Add project files.

---
+++ SRP/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    internal class Customer : ICustomer
+    {
+        public void AddCustomer()
+        {
+            try
+            {
+                // TODO: Insert Logic Need to Written
+                Console.WriteLine("Customer Added.");
+            }
+            catch (Exception ex)
+            {
+
+                ErrorLog.WriteLog(new string[] { ex.Message.ToString() });
+            }
+
+        }
+
+        public void RemoveCustomer()
+        {
+            // TODO: Remove Logic Need to Written
+            Console.WriteLine("Customer Removed.");
+        }
+
+        public void UpdateCustomer()
+        {
+            // TODO: Update Logic Need to Written
+            Console.WriteLine("Customer Removed.");
+        }
+    }
+}
---
+++ SRP/ICustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    internal interface ICustomer
+    {
+        void AddCustomer();
+
+        void RemoveCustomer();
+
+        void UpdateCustomer();
+    }
+}
